860e4620aa0d6e7c765ffee5a060a5d95eb0c4c2
ntech4 <133063198+ntech4@users.noreply.github.com>
2023-12-05 22:47:20 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+class ThriftStore {
+    constructor(store, product){
+        this.store = stores;
+        this.product = products;
+    }
+}
+//console.log(ThriftStore)
+
+class product {
+    constructor(name, type, quantity, marketPrice){
+        this.name = productName;
+        this.type = typeOfProduct;
+        this.quantity = ProductQuantity;
+        this.marketPrice = marketPricePro;
+    }
+}
+//console.log(product);
+
+let = StoreONe = {
+    location: {
+        city: "brattleboro",
+        state: 'vermont',
+    },
+    salesTax: 2.3,
+    inventory: [],
+    balance: 200,
+    Expenses: 45, 
+    profit: 0,
+    paidTax: []
+
+}
++
+//console.log(StoreONe)
+
+let = StoreTwo = {
+    location: {
+        city: "Las vegas",
+        state: 'Nevada',
+    },
+    salesTax: 3.3,
+    inventory: [],
+    balance: 130,
+    Expenses: 50, 
+    profit: 0,
+    paidTax: []
+
+}
+// console.log(StoreTwo)
+
+let = StoreThree = {
+    location: {
+        city: "Keen",
+        state: 'New hampshire',
+    },
+    salesTax: 1.3,
+    inventory: [],
+    balance: 100,
+    profit: 0,
+    Expenses: 100, 
+    paidTax: []
+
+}
+/// console.log(StoreThree)
+
